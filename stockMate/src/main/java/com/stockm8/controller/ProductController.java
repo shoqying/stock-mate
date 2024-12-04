3ec1e9dc61395e3f5bacb7ed861bd7046b0b5058
insung <in27sung@gmail.com>
2024-12-04 18:03:38 +0900

    DEBUG ReqeustMapping value

--- stockMate/src/main/java/com/stockm8/controller/ProductController.java
+++ stockMate/src/main/java/com/stockm8/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.stockm8.service.ProductService;
 
@@ -19,10 +20,12 @@ public class ProductController {
 	private ProductService pService;
 	
 	// http://localhost:8088/product/create
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
 	public void registGET() throws Exception {
 		logger.info("/product/create 호출");
 		logger.info("연결된 뷰페이지(/board/create.jsp) 이동");
 	}
+	
 	 
 	
 }
