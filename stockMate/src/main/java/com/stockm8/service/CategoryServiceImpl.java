e8ecd06b8e8b6940748b86d71ef81a30fc34645a
Insung <110476794+in27sung@users.noreply.github.com>
2024-12-09 11:09:42 +0900

    Update CategoryServiceImpl.java
    변수명 체크

--- stockMate/src/main/java/com/stockm8/service/CategoryServiceImpl.java
+++ stockMate/src/main/java/com/stockm8/service/CategoryServiceImpl.java
@@ -14,35 +14,35 @@ import com.stockm8.persistence.CategoryDAO;
 public class CategoryServiceImpl implements CategoryService {
 
     @Inject
-    private CategoryDAO cdao;
+    private CategoryDAO categoryDAO;
 
     @Override
-    public void addCategory(CategoryVO vo) throws Exception {
+    public void addCategory(CategoryVO category) throws Exception {
     	
     	System.out.println(" 카테고리 등록 실행 ");
     	
     	// 기본 businessId 설정
-        vo.setBusinessId(1);  
+        category.setBusinessId(1);  
 
         // 상위 카테고리가 없으면 대분류, 있으면 소분류로 설정
         if (vo.getParentId() == null) {
-            vo.setLevel(1);  // 대분류
+            category.setLevel(1);  // 대분류
         } else {
-            vo.setLevel(2);  // 소분류
+            category.setLevel(2);  // 소분류
         }
 
         // 현재 시간을 생성 시간으로 설정
         vo.setCreatedAt(new Timestamp(System.currentTimeMillis()));
 
     	System.out.println(" DAO의 카테고리 등록 메서드 호출");
-    	cdao.insertCategory(vo);
+    	categoryDAO.insertCategory(category);
     }
 
     @Override
     public List<CategoryVO> getAllCategories() throws Exception {
     	System.out.println(" getAllCategories() 호출 ");
     	
-        return cdao.selectAllCategories(); 
+        return categoryDAO.selectAllCategories(); 
     }
     
     // 특정 사업자(businessId) 소속의 카테고리 목록을 조회
@@ -50,26 +50,26 @@ public class CategoryServiceImpl implements CategoryService {
 	public List<CategoryVO> getCategoriesByBusinessId(int businessId) throws Exception {
     	System.out.println(" getCategoriesByBusinessId() 호출 ");
 
-    	return cdao.selectCategoriesByBusinessId(businessId);
+    	return categoryDAO.selectCategoriesByBusinessId(businessId);
 	}
     
     // 카테고리 수정
     @Override
-    public void updateCategory(CategoryVO vo) throws Exception {
+    public void updateCategory(CategoryVO category) throws Exception {
         // 카테고리 수정
-    	cdao.updateCategory(vo);
+    	categoryDAO.updateCategory(category);
     }
 
 	// 카테고리와 상위 카테고리 정보 조회
     @Override
     public CategoryVO getCategoryWithParents(int cId) throws Exception {
         // 카테고리 정보 조회
-        CategoryVO vo = cdao.selectCategoryById(cId);
+        CategoryVO vo = categoryDAO.selectCategoryById(cId);
 
         // 상위 카테고리 조회
         CategoryVO parentCategory = null;
         if (vo.getParentId() != null) {
-            parentCategory = cdao.selectCategoryById(vo.getParentId());  // vo.getParentId() 사용
+            parentCategory = categoryDAO.selectCategoryById(vo.getParentId());  // vo.getParentId() 사용
         }
 
         // 상위 카테고리 정보를 별도로 처리 (VO에 저장하지 않음)
@@ -80,7 +80,7 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public void deleteCategory(int cId) throws Exception {
     	// 카테고리 삭제
-    	cdao.deleteCategory(cId);
+    	categoryDAO.deleteCategory(cId);
     }
 
 
