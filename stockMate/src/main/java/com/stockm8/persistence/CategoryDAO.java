bd3295cc009ec8197fa600c4cd6967b7775e6e6d
Insung <110476794+in27sung@users.noreply.github.com>
2024-12-10 12:18:36 +0900

    Update CategoryDAO.java

--- stockMate/src/main/java/com/stockm8/persistence/CategoryDAO.java
+++ stockMate/src/main/java/com/stockm8/persistence/CategoryDAO.java
@@ -8,7 +8,6 @@ import com.stockm8.domain.vo.CategoryVO;
 
 public interface CategoryDAO {
 
-    
     // 카테고리 등록
     public void insertCategory(CategoryVO vo) throws Exception;
     
@@ -29,30 +28,5 @@ public interface CategoryDAO {
     
     // 카테고리 삭제
     public void deleteCategory(@Param("categoryId") int categoryId) throws Exception;
-
-
-  // 카테고리 등록
-  public void insertCategory(CategoryVO vo) throws Exception;
-
-  // 카테고리 목록 조회
-  public List<CategoryVO> selectAllCategories() throws Exception;
-
-  // 특정 사업자(businessId) 소속의 카테고리 목록 조회
-  public List<CategoryVO> selectCategoriesByBusinessId(@Param("businessId") int businessId) throws Exception;
-
-  // 카테고리ID로 카테고리명 조회
-  public String selectCategoryNameById(int categoryId) throws Exception;
-
-  // 카테고리 목록 유효성 조회
-  boolean existsById(int categoryId, int businessId);
-
-  // 카테고리 수정
-  public void updateCategory(CategoryVO vo) throws Exception;
-
-  // ID로 특정 카테고리 조회
-  public CategoryVO selectCategoryById(int catergoryId) throws Exception;
-
-  // 카테고리 삭제
-  public void deleteCategory(int cId) throws Exception;
 
 }
